004797ef4a7e10cfa87ac37ef78209ac18979f9d
Hyungon_Ryu <yhgon@users.noreply.github.com>
2018-10-10 15:56:14 +0900

    Update acgan.py
    add 
    import sys,os
    sys.path.append(os.getcwd())

--- acgan/acgan.py
+++ acgan/acgan.py
@@ -1,4 +1,6 @@
 from __future__ import print_function, division
+import sys,os
+sys.path.append(os.getcwd())
 
 from keras.datasets import mnist
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply
