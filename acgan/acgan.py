fafe5edd59d6bb73b68c5503dd24e4452d18a0d4
Hyungon_Ryu <yhgon@users.noreply.github.com>
2018-10-10 17:46:00 +0900

    Update acgan.py
    /content/Keras-GAN/acgan/

--- acgan/acgan.py
+++ acgan/acgan.py
@@ -190,7 +190,7 @@ class ACGAN():
                 axs[i,j].imshow(gen_imgs[cnt,:,:,0], cmap='gray')
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("images/%d.png" % epoch)
+        fig.savefig("/content/Keras-GAN/acgan/images/%d.png" % epoch)
         plt.close()
 
     def save_model(self):
